f6d27cd1b768baed6456981096b682758db2870e
CurtisChou-51 <71554373+CurtisChou-51@users.noreply.github.com>
2026-05-05 01:01:05 +0800

    feat: 外層結構

---
+++ PatternFileCategorizer/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace PatternFileCategorizer
+{
+    public class AppConfig
+    {
+        public List<string> Patterns { get; set; } = new();
+        public string SourceDirectory { get; set; } = "";
+        public string TargetDirectory { get; set; } = "";
+    }
+}
---
+++ PatternFileCategorizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
+
+namespace PatternFileCategorizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var config = configuration.Get<AppConfig>() ?? new AppConfig();
+            if (string.IsNullOrEmpty(config.SourceDirectory) || !Directory.Exists(config.SourceDirectory))
+            {
+                Console.WriteLine("來源路徑無效。");
+                return;
+            }
+
+            List<Regex> regexes = config.Patterns.Select(p => new Regex(p, RegexOptions.Compiled)).ToList();
+            IEnumerable<string> files = Directory.EnumerateFiles(config.SourceDirectory, "*.*", SearchOption.AllDirectories);
+
+            Console.WriteLine($"[開始處理] 來源: {config.SourceDirectory}");
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file);
+                // todo
+            }
+        }
+    }
+}
